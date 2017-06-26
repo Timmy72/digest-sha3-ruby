72394e2e6a5f0f6d548c32c0f9b73f044cfde1e7
Timmy72 <vincent.renaudineau@gmail.com>
2017-06-26 15:30:25 +0200

    Remove warning errors

--- ext/digest/extconf.rb
+++ ext/digest/extconf.rb
@@ -3,5 +3,10 @@ require 'mkmf'
 have_header('ruby/digest.h')
 have_func('rb_str_set_len')
 
-$CFLAGS << " -fvisibility=hidden"
+$CFLAGS << " -fvisibility=hidden" << " -Wno-error"
+$CFLAGS.gsub!("-Werror=format-security","")
+
+$CXXFLAGS << " -Wno-error"
+$CXXFLAGS.gsub!("-Werror=format-security","")
+
 create_makefile('digest/sha3')
